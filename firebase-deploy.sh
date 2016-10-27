a72f164fb72e62de4d703c07e73af387c93b603c
Francisco Baena <baena.francisco@gmail.com>
2016-10-28 00:02:26 +0200

    Testing Firebase auto deploy

---
+++ firebase-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+set -o pipefail
+
+if [ "$TRAVIS_BRANCH" = "master" ] #&& [ "$TRAVIS_PULL_REQUEST" = "false" ]
+then
+  polymer test
+  git config --global user.email "baena.francisco@gmail.com"
+  git config --global user.name "travis auto deployer"
+  # Stamp index.html with the date and time of PSK's deploying
+  #date_value=`date`
+  #sed -i.tmp1 "s/This is another card./This is another card. PSK Deployed at: $date_value/" app/index.html
+  # Building Polymer
+  polymer build
+  # Starting Deploy Process to Firebaseapp.com
+  firebase deploy --token "${FIREBASE_TOKEN}"
+
+else
+  polymer lint
+  polymer test
+fi
